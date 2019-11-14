647e43384d77248a186ba388d6a28f1f19cffb81
Mikey Rivera <mikeyriver2@gmail.com>
2019-11-15 03:19:53 +0800

    add reservation id to log

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -11,19 +11,19 @@ class ReservationsController extends Controller
         $inputs = $request->all();
         $reservation = Reservation::create($inputs);
         try{
-            $this->logAction($request);
+            $this->logAction($request,$reservation);
         }catch(Exception $e){
             //do nothing, continue
         }
         return $reservation;
     }
 
-    public function logAction($request){
+    public function logAction($request, $reservation){
         $data = $request->client["data"];
         $value = [
             'ip_address' => $data["ip"],
             'user_agent' => $request->client["userAgent"],
-            'action' => $request->client["action"],
+            'action' => "".$request->client["action"]." reservation id: ".$reservation->id."",
             'coordinates' => $data["loc"],
             'isp' => $data["org"],
             'post_code' => $data["postal"],
